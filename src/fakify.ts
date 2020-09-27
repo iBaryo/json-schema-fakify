6e42026adb55fd95a8b8898a6b6661c6f889aa8f
baryo <baryo.stuff@gmail.com>
2020-09-27 03:19:46 +0300

    refactoring

--- src/fakify.ts
+++ src/fakify.ts
@@ -1,53 +1,54 @@
 import {JSONSchema7} from "json-schema";
-import * as fakerLib from "faker";
+import * as fakerLibFull from "faker";
 import {singular} from "pluralize";
 import {defaultSchemaPropFakers} from "./DefaultPropFakers";
 
+const fakerLib = Object.fromEntries(
+    Object.entries(fakerLibFull)
+        .filter(([categoryName, category]) =>
+            categoryName != 'definitions' && typeof category != 'function') // faker internals
+);
+
 export function fakify(schema: JSONSchema7, propName?: string) {
+    return {
+        ...schema,
+        ...enhanceFaker(schema, propName)
+    };
+}
+
+function enhanceFaker(schema: JSONSchema7, propName: string) {
     if (schema.type == 'array') {
         return {
-            ...schema,
-            ...{
-                items:
-                    typeof schema.items == 'boolean' ? schema.items :
-                        schema.items instanceof Array ?
-                            schema.items.map(i => typeof i == 'boolean' ? i : fakify(i, propName))
-                            : fakify(schema.items, propName)
-            }
+            items: typeof schema.items == 'boolean' ? schema.items :
+                    schema.items instanceof Array ?
+                        schema.items.map(subSchema => typeof subSchema == 'boolean' ? subSchema : fakify(subSchema, propName))
+                        : fakify(schema.items, propName)
         };
     } else if (schema.type != 'object') {
-        return {
-            ...schema,
-            ...getFaker(propName, schema)
-        };
+        const faker = getFakerName(schema, propName);
+        return !faker ? undefined : {faker};
     } else {
         return {
-            ...schema,
-            ...{
-                properties: Object.fromEntries(
-                    Object.entries(schema.properties)
-                        .map(([propName, schema]) => [
-                            propName,
-                            typeof schema == 'boolean' ? schema : fakify(schema, propName)
-                        ])
-                )
-            }
+            properties: Object.fromEntries(
+                Object.entries(schema.properties)
+                    .map(([propName, schema]) => [
+                        propName,
+                        typeof schema == 'boolean' ? schema : fakify(schema, propName)
+                    ])
+            )
         };
     }
 }
 
-function getFaker(propName: string, schema: JSONSchema7) {
+function getFakerName(schema: JSONSchema7, propName: string) {
     propName = singular(propName || '');
     const faker = schema['faker'] || getFakerLibName(defaultSchemaPropFakers[propName] || propName);
-    return !faker ? undefined : {faker};
+    return faker;
 }
 
 function getFakerLibName(prop: string) {
     const [categoryName] =
-        Object.entries(fakerLib)
-            .filter(([categoryName, category]) =>
-                categoryName != 'definitions' && typeof category != 'function') // faker internals
-            .find(([_, category]) => category[prop]) || [];
+        Object.entries(fakerLib).find(([_, category]) => category[prop]) || [];
     return categoryName ? `${categoryName}.${prop}` : undefined;
 }
 
